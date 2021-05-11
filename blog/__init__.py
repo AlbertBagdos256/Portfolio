2e3c6e9fbbe05b46ebee23ce6c7ffa3f9b4d1637
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2021-05-11 18:56:39 +0500

    Add files via upload

---
+++ blog/__init__.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_fontawesome import FontAwesome
+from blog.config import Configuration
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+fa = FontAwesome(app)
+app.config.from_object(Configuration)
+
+# Databse
+app.secret_key = 'hn23Lop3781456gs8ghr74yr8hh73ABH789y83fGn23hBJOP123DBGshAlolHjU762FRhjhvcb3738yyfgb3fhif74uibuy8huibfh8yug8ihf8j389unhhf8u8'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/Blog_Flask"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from blog import routes

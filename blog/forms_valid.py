0a08c98cacfe01befac3d070a541f6842cef59b9
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2021-06-05 15:15:23 +0500

    Update forms_valid.py

--- blog/forms_valid.py
+++ blog/forms_valid.py
@@ -3,6 +3,7 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     

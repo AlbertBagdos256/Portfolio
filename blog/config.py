83a5ad4d9434020a5b16ce0141651d0235244906
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2021-06-12 00:23:14 +0500

    Update config.py

--- blog/config.py
+++ blog/config.py
@@ -2,5 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = 'hn23Lop3781456gs8ghr74yr8hh73ABH789y83fGn23hBJOP123DBGshAlolHjU762FRhjhvcb3738yyfgb3fhif74uibuy8huibfh8yug8ihf8j389unhhf8u8'
-    SQLALCHEMY_DATABASE_URI =  "sqlite:///site.db"    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False   

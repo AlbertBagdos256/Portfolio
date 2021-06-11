f235486166fd9aed5a17ff378f3155b10c476ef6
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2021-06-12 02:23:36 +0500

    Update config.py

--- blog/config.py
+++ blog/config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'h48gh423h0gh894hgh49gh2pg9u0923h84g'
     SQLALCHEMY_TRACK_MODIFICATIONS = False   

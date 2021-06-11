4911aa5ad1571ff3eb80a6cfb2346a3114c3b4e4
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2021-06-11 19:33:56 +0500

    Update __init__.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -11,7 +11,7 @@ app.config.from_object(Configuration)
 
 # Databse
 app.secret_key = 'hn23Lop3781456gs8ghr74yr8hh73ABH789y83fGn23hBJOP123DBGshAlolHjU762FRhjhvcb3738yyfgb3fhif74uibuy8huibfh8yug8ihf8j389unhhf8u8'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/Blog_Flask"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 SQLAlCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)

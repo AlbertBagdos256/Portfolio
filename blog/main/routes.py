e8a91d07a6be397e89db4e6bb1fbede2efb03eb7
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2021-06-13 04:00:47 +0500

    Update routes.py

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template, request, Blueprint
+from blog.models import User
+from blog.main.utils import get_users_urls,get_users_avatars,get_main_list
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods = ['POST','GET'])
 def index():
-    return render_template('index.html')  
+    users = User.query.all()
+    images_list = get_users_avatars(users)
+    users_urls  = get_users_urls(users)
+    main_list   = get_main_list(images_list, users_urls)
+    return render_template('index.html', main_list = main_list) 
